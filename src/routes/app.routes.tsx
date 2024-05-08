bf4a671457a180d0d5502ea989c2d6d9fec40cf7
Antônio Sales <antoniosales1003@gmail.com>
2024-05-08 10:53:47 -0300

    feat: add bottomTabs style

--- src/routes/app.routes.tsx
+++ src/routes/app.routes.tsx
@@ -1,3 +1,4 @@
+import { Platform } from 'react-native';
 import { Home } from '@/screens/Home';
 import { History } from '@/screens/History';
 import { Profile } from '@/screens/Profile';
@@ -5,6 +6,9 @@ import {
   createBottomTabNavigator,
   BottomTabNavigationProp,
 } from '@react-navigation/bottom-tabs';
+import { MaterialCommunityIcons } from '@expo/vector-icons';
+import { Ionicons } from '@expo/vector-icons';
+import colors from 'tailwindcss/colors';
 
 type AppRoutes = {
   home: undefined;
@@ -14,13 +18,56 @@ type AppRoutes = {
 
 export type AppNavigatorRoutesProps = BottomTabNavigationProp<AppRoutes>;
 const { Navigator, Screen } = createBottomTabNavigator<AppRoutes>();
+const iconSize = 24;
 
 export function AppRoutes() {
   return (
-    <Navigator>
-      <Screen name="home" component={Home} />
-      <Screen name="history" component={History} />
-      <Screen name="profile" component={Profile} />
+    <Navigator
+      screenOptions={{
+        headerShown: false,
+        tabBarShowLabel: false,
+        tabBarActiveTintColor: colors.green[500],
+        tabBarInactiveTintColor: colors.gray[300],
+        tabBarStyle: {
+          backgroundColor: colors.black[300],
+          borderWidth: 0,
+          height: Platform.OS === 'android' ? 70 : 96,
+          marginBottom: 30,
+          marginTop: 24,
+        },
+      }}
+    >
+      <Screen
+        name="home"
+        component={Home}
+        options={{
+          tabBarIcon: ({ color }) => (
+            <MaterialCommunityIcons name="home" size={iconSize} color={color} />
+          ),
+        }}
+      />
+      <Screen
+        name="history"
+        component={History}
+        options={{
+          tabBarIcon: ({ color }) => (
+            <MaterialCommunityIcons
+              name="finance"
+              size={iconSize}
+              color={color}
+            />
+          ),
+        }}
+      />
+      <Screen
+        name="profile"
+        component={Profile}
+        options={{
+          tabBarIcon: ({ color }) => (
+            <Ionicons name="person-circle" size={iconSize} color={color} />
+          ),
+        }}
+      />
     </Navigator>
   );
 }
